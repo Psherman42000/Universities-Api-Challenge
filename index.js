132b003abb5762c7e57186ed9d3d483a7fd6cea1
Pedro C. Dias <pedroid199@gmail.com>
2022-03-09 01:08:58 -0300

    [UPDATE] odm is progress

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const UniversitiesPiker = new require('./controllers/universities-piker/UniversitiesPiker')
-const Odm = new require('./model/Odm')
+let UniversitiesPiker = require('./controllers/universities-piker/UniversitiesPiker')
+let Odm = require('./model/Odm')
 
 const start = async () => {
+    initializeDependencies()
     await hydrateDbWithApiData()
 }
 
+initializeDependencies = () => {
+    Odm = new Odm()
+    UniversitiesPiker = new UniversitiesPiker()
+}
+
 const hydrateDbWithApiData = async () => {
+    await Odm.initializeDb()
     if(await Odm.alreadyHydrated()) return
-    const universities = await UniversitiesPiker.getUniversitiesFromApi()
-    return await Promise.all(universities.map(async (university) => {
-        await Odm.crate(university)
-    }))
+    console.log('[INFO] first time runnig server, hydrating database with universities data...')
+    try{
+        const universities = await UniversitiesPiker.getUniversitiesFromApi()
+        await Odm.insertMany(universities)
+    }catch(error){
+        console.log('[ERROR] Error Ocourred trying hydrate database')
+        throw error
+    }
+    console.log('[INFO] Database hydrated sucessfully')
 }
 
 start()
--- model/Odm.js
+++ model/Odm.js
@@ -3,11 +3,62 @@
 module.exports = class Odm {
   constructor() {
     this._mongo = require('mongoose')
-    this.connect = this._api().connect
-    this.connection = this._api().connection
-    this.disconnect = this._api().disconnect
-    this.model = this._api().model
-    this.Schema = this._api().Schema
+    this._dbUri = require('./dbData.json').uri
+    this._connected = false
+    this._model
   }
+
+  initializeDb = async () => {
+    if(this._connected) return
+    console.log('[INFO] Connecting with Database')
+    try{
+      await this._mongo.connect(this._dbUri)
+    }catch(error){
+      console.log('[ERROR] Connection with Database Failed')
+      throw error
+    }
+    this._connected = true
+    console.log('[INFO] Sucessfully Connected with Database')
+  }
+
+  alreadyHydrated = async () => {
+    await this.initializeDb()
+    const model = this._model ? this._model : this._createModel()
+    const result = await model.findOne({})
+    if(result.length === 0) return false
+    return true
+  }
+
+  _createModel = () => {
+    try {
+      return (this._model = this._mongo.model('Universities'))
+    } catch {
+      const schema = this._createSchema()
+      return (this._model = this._mongo.model('Universities', schema, 'Universities'))
+    }
+  }
+
+  _createSchema = () => {
+    const schema = {
+      country: String,
+      name: String,
+      domains: Array,
+      alpha_two_code: String,
+      'state-province': 'Mixed',
+      web_pages: Array
+    }
+    const opts = {
+      timestamps: true,
+      strict: false,
+      minimize: false,
+    }
+    return this._mongo.Schema(schema, opts)
+  }
+
+  insertMany = async (data) => {
+    await this.initializeDb()
+    const model = this._model ? this._model : this._createModel()
+    return await model.create(data)
+  } 
 
 }
