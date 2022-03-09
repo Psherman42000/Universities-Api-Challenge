256c2fd618c5d3cf629b6bab00b91390a53e87ab
Pedro <pedroid199@gmail.com>
2022-03-09 18:28:21 -0300

    [UPDATE] query improvement

--- controllers/universities-manager/UniversitiesManager.js
+++ controllers/universities-manager/UniversitiesManager.js
@@ -6,17 +6,17 @@ module.exports = class UniversitiesManager {
   }
 
   getUniversitiesFromCountry = async ({ country, allRecords }) => {
+    const query = {
+      country: {
+        "$regex": country,
+        "$options": "i"
+      }
+    }
     let queryResult
     if(allRecords)
-      queryResult = await this._model.find({country: {
-        "$regex": country,
-        "$options": "i"
-    }})
+      queryResult = await this._model.find(query)
     else
-      queryResult = await this._model.find({country: {
-        "$regex": country,
-        "$options": "i"
-    }}).limit(20)
+      queryResult = await this._model.find(query).limit(20)
     return queryResult.map(university => {
       const dataTransferObject = {
         _id: university._id,
